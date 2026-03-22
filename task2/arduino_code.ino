3b82c9428c9c2e5dbd05aa16f70cd5444af83a48
jeffinfrs <jeffinfr@gmail.com>
2026-03-22 17:16:43 +0530

    Added Arduino Code

---
+++ task2/arduino_code.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo myServo;
+
+int potPin = A0;
+int ledPin = 7;
+int servoPin = 9;
+
+void setup() {
+  myServo.attach(servoPin);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  int angle = map(potValue, 0, 1023, 0, 180);
+
+  if (angle > 68) {
+    myServo.write(68);
+    digitalWrite(ledPin, HIGH);
+  } else {
+    myServo.write(angle);
+    digitalWrite(ledPin, LOW);
+  }
+}
